71098b48e83cd3f6e3a741464b038e6988048dd7
Rob Vitaro <robvitaro@gmail.com>
2020-09-07 09:31:28 -0400

    progress_notes#new is root; notes is text_area; patient is select field

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :progress_notes
   resources :patients
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root 'progress_notes#new'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Patient.create(name: 'Cierra Vega')
+Patient.create(name: 'Alden Cantrell')
+Patient.create(name: 'Kierra Gentry')
+Patient.create(name: 'Pierre Cox')
+Patient.create(name: 'Thomas Crane')
+Patient.create(name: 'Miranda Shaffer')
+Patient.create(name: 'Bradyn Kramer')
+Patient.create(name: 'Alvaro Mcgee')
