6645b6bee8ae006add37493d4b966d2eb910d002
Rob Vitaro <robvitaro@gmail.com>
2020-09-07 12:59:29 -0400

    notes parser can extract scores

--- app/services/notes_parser.rb
+++ app/services/notes_parser.rb
@@ -1,73 +1,100 @@
 class NotesParser
-  attr_reader :notes
+  attr_reader :notes, :errors, :scores
 
   def initialize(progress_note)
-    @notes =progress_note.notes
+    @notes = progress_note.notes
   end
 
   def call
     scanner = StringScanner.new(notes)
-    find_errors(scanner)
-
+    @scores = find_scores(scanner)
+    @errors = find_errors(scanner)
   end
 
   private
+
+  def find_scores(scanner)
+    scanner.reset
+    scores = {}
+
+    if scanner.exist?(/\+|-/)
+      until scanner.eos?
+        word = scanner.scan_until(/\+|-/)
+        word = word&.chop&.chop&.gsub("\n","") # Ewwww!!
+        scanner.pos = scanner.pos - 1
+        score_string = scanner.scan_until(/$/)
+        scores[word] = get_score(score_string)
+      end
+    end
+
+    scores
+  end
+
+  def get_score(string)
+    return unless string
+
+    wrong = string.scan(/-/).length
+    right = string.scan(/\+/).length
+    right.to_f / (right.to_f + wrong.to_f)
+  end
 
   def find_errors(scanner)
     scanner.reset
     errors = []
 
-    until scanner.eos?
-      scanner.scan_until(/errors: /)
-      scanner.skip(/errors: /)
-      errors_string = scanner.scan_until(/$/)
-      errors << errors_string.split(', ')
+    if scanner.exist?(/errors: /)
+      until scanner.eos?
+        scanner.scan_until(/errors: /)
+        scanner.skip(/errors: /)
+        errors_string = scanner.scan_until(/$/)
+        errors << errors_string.split(', ')
+      end
     end
 
     errors.flatten
   end
 
-    #
-    # /sh/ IWP imitative -++--+++-+ 6/10 60%
-    # errors: sheep, she, shape
-    #
-    # /sound/ abbrv-task tasksubtype score
-    # error list
-    #
-    #
-    #
-    #
-    # Expressive/Pragmatic Language - book (the tiny seed)
-    # STG2: named vocabulary in pictures -visual/verbal/max cues
-    # sun -+
-    # fish +
-    # seed +
-    # bird - -
-    # grass +
-    # snow -
-    # rain -
-    # foot -
-    # flower - -
-    # tree +
-    # Total: 5/13=38%
-    #
-    # task tasksubtype
-    # goal
-    # word score
-    # word score
-    #
-    #
-    #
-    #
-    # STG3: labeled vocabulary (nouns, verbs) -visual/mod cues:
-    # nouns: +++++ +++ 8/8
-    # errors: 0
-    # verbs: ++ - - - - + 3/7
-    # errors: swim, kick, catch, throw
-    #
-    # goal
-    # word score
-    # error list
-    # word score
-    # error list
+  #
+  # /sh/ IWP imitative -++--+++-+ 6/10 60%
+  # errors: sheep, she, shape
+  #
+  # /sound/ abbrv-task tasksubtype score
+  # error list
+  #
+  #
+  #
+  #
+  # Expressive/Pragmatic Language - book (the tiny seed)
+  # STG2: named vocabulary in pictures -visual/verbal/max cues
+  # sun -+
+  # fish +
+  # seed +
+  # bird - -
+  # grass +
+  # snow -
+  # rain -
+  # foot -
+  # flower - -
+  # tree +
+  # Total: 5/13=38%
+  #
+  # task tasksubtype
+  # goal
+  # word score
+  # word score
+  #
+  #
+  #
+  #
+  # STG3: labeled vocabulary (nouns, verbs) -visual/mod cues:
+  # nouns: +++++ +++ 8/8
+  # errors: 0
+  # verbs: ++ - - - - + 3/7
+  # errors: swim, kick, catch, throw
+  #
+  # goal
+  # word score
+  # error list
+  # word score
+  # error list
 end
--- test/services/notes_parser_test.rb
+++ test/services/notes_parser_test.rb
@@ -4,6 +4,18 @@ class NotesParserTest < ActiveSupport::TestCase
   test "it extracts list of errors" do
     notes = "errors: sheep, she, shape\nerrors: swim, kick, catch, throw"
     parser = NotesParser.new(ProgressNote.new(notes: notes))
-    assert_equal %w[sheep she shape swim kick catch throw], parser.call
+    parser.call
+    assert_equal %w[sheep she shape swim kick catch throw], parser.errors
+  end
+
+  test "it extracts scores" do
+    notes = "/sh/ IWP imitative -++--+++-+ 6/10 60%\n/ch/ IWP imitative +++-++++-+ 8/10 80%"
+    parser = NotesParser.new(ProgressNote.new(notes: notes))
+    parser.call
+    expected = {
+        "/sh/ IWP imitative" => 0.6,
+        "/ch/ IWP imitative" => 0.8
+    }
+    assert_equal expected, parser.scores
   end
 end
